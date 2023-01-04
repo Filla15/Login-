f14fb67e4b803b4852d7e3d47bfe69c37cc2c121
Filla Firontina <120008072+Filla15@users.noreply.github.com>
2023-01-04 19:39:37 +0700

    Daftar_user.php

---
+++ daftar_user.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Input Data Mahasiswa</title>
+</head>
+<body>
+	DAFTAR LOGIN 
+		<form action ="simpan_user.php" method="post">
+			<table>
+			<tr><td>NAMA PENGGUNA</td><td>:</td>
+				<td><input type="text" name="Nama"></td>
+			</tr>
+			<tr><td>USERNAME</td><td>:</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr><td>PASSWORD</td><td>:</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td colspan="3" align="center">
+					<input type="submit" value="Daftar">
+					<input type="reset" value="batal">
+				</td>
+			</tr>
+			</table>
+		</form>
+</body>
+</html>
